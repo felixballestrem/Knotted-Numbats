6741c81bcdf1a2722299e2d740f3fec8032336b9
risons <lokugamhewar@gmail.com>
2025-11-26 20:04:43 +0000

    started writing smoothing
    Refactor kauffman function to improve readability and structure.

--- kauffman.py
+++ kauffman.py
@@ -1,19 +1,30 @@
+from sympy import symbols
 def kauffman(crossings):
     if len(crossings) == 0:
+        #trivial case
         return 1
-    test = crossings
     while len(crossings) != 0:
-        cross = test[0]
+        #working cross by cross
+        cross = crossings[0]
+        #initiallising each element of cross
         a = cross[0]
         b = cross[1]
         c = cross[2]
         d = cross[3]
-        e = test.pre(a)
-        h = test.succ(d)
+        #initiallising the crossing that each element links to
+        E = crossings.preCross(a)
+        G = test.sucCross(c)
         if crossingSign == 1:
-            f = test.pre(c)
-            g = test.succ(d)
+            F = crossings.preCross(b)
+            H = crossings.sucCross(d)
         else:
-            f = test.succ(c)
-            g = test.pre(d)
+            f = crossings.sucCross(b)
+            h = crossings.preCross(d)
+        testA = aSmoothing(crossings,a,b,c,d,E,F,G,H)
+        testB = bSmoothing(crossings,a,b,c,d,E,F,G,H)
+        return 
+        
+def aSmoothing(crossings,a,b,c,d,E,F,G,H):
+    crossings.index(E)
+        
         
