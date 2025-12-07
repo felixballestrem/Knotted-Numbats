1cd2f3b4e3a48d91d28605da2828b3577a3d1ef1
risons <lokugamhewar@gmail.com>
2025-12-07 16:13:11 +0000

    Completed Kauffman and Jones polynomial
    Refactor kauffman function and related methods for better readability and performance. Introduce connections list for indexing and streamline loop handling.

--- kauffman.py
+++ kauffman.py
@@ -2,50 +2,46 @@ from sympy import symbols,simplify
 
 A= symbols('A')
 def kauffman(crossings):
+    if len(crossings) == 0:
+        #trivial case
+        return 1
     #working cross by cross
     cross = crossings[0]
     #initiallising each element of cross
     a,b,c,d = cross
-    
-    #initiallising the index of the crossing that each element links to
-    E = crossings.index(preCross(crossings,a))
-    G = crossings.index(sucCross(crossings,c))
-    if crossingSign(cross) == 1:
-        F = crossings.index(preCross(crossings,b))
-        H = crossings.index(sucCross(crossings,d))
-    else:
-        F = crossings.index(sucCross(crossings,b))
-        H = crossings.index(preCross(crossings,d))
-            #skein moves
+    connections = [0 if len(arcConnect(crossings,i)) ==1 else crossings.index(arcConnect(crossings,i)[0]) if arcConnect(crossings,i)[1] == cross else crossings.index(arcConnect(crossings,i)[1]) for i in cross]
+    E,F,G,H = connections
     testA = aSmoothing(crossings,a,b,c,d,E,F,G,H)
     testB = bSmoothing(crossings,a,b,c,d,E,F,G,H)
-    if len(crossings) == 0 or a == b and b == c and c == d:
-        #trivial case
-        return 1    
-    elif G==0 and H ==0 or E == 0 and F == 0: # if the western or eastern arcs are loops        
-        return simplify(kauffman(bSmoothing(crossings,a,b,c,d,E,F,G,H)) * (-A**1 - A**-3) + A* kauffman(testA) )
-    elif G==0 and F ==0 or E == 0 and H == 0 : # if the northern or southern arcs are loops    
-        return simplify(kauffman(aSmoothing(crossings,a,b,c,d,E,F,G,H)) * (-A**3 - A**-1) + (A**-1)* kauffman(testB))
+    if E==0 and F == 0 and G == 0 and H== 0:
+        # if both arcs are loops
+        if crossingSign(cross,crossings) == 1: # if the western and eastern arcs are loops        
+            return -A **-3
+        else: 
+            return -A**3     # if the northern and southern arcs are loops        
+    elif (G==0 and H ==0) or (E == 0 and F == 0): # if the western or eastern arcs are loops        
+        return simplify(kauffman(testB) * (-A**1 - A**-3) + A* kauffman(testA) )
+    elif (G==0 and F ==0) or (E == 0 and H == 0): # if the northern or southern arcs are loops    
+        return simplify(kauffman(testA) * (-A**3 - A**-1) + (A**-1)* kauffman(testB))
     else:
         return simplify(A* kauffman(testA) + (A**-1)* kauffman(testB))
     
 def aSmoothing(originalcrossings,a,b,c,d,E,F,G,H):
     #connecting a (se) to d (sw) and b (ne) to c (nw)
     crossings = [crossing[:] for crossing in originalcrossings]
-    if G != 0 or F != 0: 
+    if G != 0: 
         g = crossings[G]
-        g[g.index(c)] = b
-    if E!=0 or H !=0: 
+        if crossingSign([a,b,c,d],crossings) ==-1:
+            g[g.index(c)] = b
+        else:
+            g[g.index(b)] = c
+        
+    if H !=0: 
         h = crossings[H]
         h[h.index(d)] = a
     del crossings[0]
     #relabeling
-    arcs = []
-    for crossing in crossings: 
-        for x in crossing:
-            if x not in arcs: 
-                arcs.append(x)
-    arcs.sort()
+    arcs = sorted({x for crossing in crossings for x in crossing})
     mapping = {old:new+1 for new,old in enumerate(arcs)}
     crossings = [[mapping[x] for x in crossing] for crossing in crossings]
     return crossings
@@ -53,36 +49,65 @@ def aSmoothing(originalcrossings,a,b,c,d,E,F,G,H):
 def bSmoothing(originalcrossings,a,b,c,d,E,F,G,H):
     #connecting a (se) to b (ne) and d (sw) to c (nw)
     crossings = [crossing[:] for crossing in originalcrossings]
-    if E != 0 or F != 0: 
+    if F != 0: 
         f = crossings[F]
         f[f.index(b)] = a
-    if G !=0 or  H !=0: 
+    if H !=0: 
         h = crossings[H]
         h[h.index(d)] = c
     del crossings[0]
     #relabeling
-    arcs = []
-    for crossing in crossings: 
-        for x in crossing:
-            if x not in arcs: 
-                arcs.append(x)
-    arcs.sort()
+    arcs = sorted({x for crossing in crossings for x in crossing})
     mapping = {old:new+1 for new,old in enumerate(arcs)}
     crossings = [[mapping[x] for x in crossing] for crossing in crossings]
     return crossings
-
-def crossingSign(cross):
+def links(crossings):
+    linkList = []
+    arcs = sorted({x for crossing in crossings for x in crossing})
+    visited = set()
+    for arc in arcs:
+        if arc in visited:
+            continue
+        i = arc
+        link = []
+        while i not in link: 
+            visited.add(i)
+            link.append(i)
+            for cross in crossings:
+                if i in cross:
+                    a,b,c,d = cross
+                    if i ==a:
+                        iTemp = c
+                    elif i ==b:
+                        iTemp = d
+                    elif i ==c:
+                        iTemp = a
+                    else:
+                        iTemp = b
+                    if iTemp not in link:
+                        i = iTemp
+                        break
+        linkList.append(link)
+    return(linkList)
+def crossingSign(cross,crossings):
     #assuming a is se, b is ne, c is nw and d is sw.
     a,b,c,d = cross
-    if d == 1 or c ==1:
-        if (c-a)*(b-d)>0:
+    linkList = links(crossings)
+    bLink = [link for link in linkList if b in link][0]
+    if bLink.index(b) == 0:
+        if (b-d)*(c-a)>0:
             return -1
         else:
             return 1
     else:
-        if (c-a)*(b-d)>0:
+        if (b-d)*(c-a)>0:
             return 1
         else:
             return -1
 def writhe(crossings):
-    return sum([crossingSign(cross) for cross in crossings])        
+    return sum([crossingSign(cross,crossings) for cross in crossings])        
+def arcConnect(crossings,arc):
+    return [cross for cross in crossings if arc in cross]
+def Jones(crossings):
+    print(kauffman(crossings),writhe(crossings))
+    return simplify((-A**3)**(writhe(crossings))* kauffman(crossings))
