bb3b9f125bf8ca153a0f04f80c915a291e529d2c
Z-Neutrino <69902408+Z-Neutrino@users.noreply.github.com>
2025-12-18 06:41:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ class Diagram:
         self.components = self.get_components()
         
         self.succs = {}
+        self.arc_comps = {}
+        
         for comp in self.components:
             for i in range(len(comp)):
                 self.succs[comp[i-1]] = comp[i]
+                self.arc_comps[comp[i]] = comp
+        
     
-      
-    def succ(self, arc):
-        return self.succs[arc]
-
-
-    def sign(self,crossing):
-        if self.succ(crossing[3]) == crossing[1]:
-            if self.succ(crossing[1]) == crossing[3]:
+    def get_sign(self,crossing):
+        if self.get_arc_succ(crossing[3]) == crossing[1]:
+            if self.get_arc_succ(crossing[1]) == crossing[3]:
                 if crossing[2] == crossing[3]:
                     return 1
                 else:
@@ -27,7 +26,27 @@ class Diagram:
         else:
             return -1
 
-  
+        
+    def writhe(self):
+        return sum([self.get_sign(c) for c in self.pd])
+
+
+    def get_linking_number(self):
+        acc = 0
+        for crossing in self.pd:
+            if get_arc_comp(crossing[0]) != get_arc_comp(crossing[1]):
+                result += get_arc_sign(crossing)
+        return 1/2 * acc
+                
+                
+    def get_arc_succ(self, arc):
+        return self.succs[arc]
+    
+    
+    def get_arc_comp(self,arc):
+        return self.arc_comps[arc]
+    
+    
     def get_components(self):
         """Determines the components of a PD
            Returns a list of lists of (adjacently ordered) arc labels """
@@ -59,12 +78,6 @@ class Diagram:
                     if nbr not in visited:
                         visited.add(nbr)
                         stack.append(nbr)
-            components.append(knot)
+            components.append(tuple(knot))
 
         return components
-    
-    
-        
-    
-        
-        
